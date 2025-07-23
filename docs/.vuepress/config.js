fec63686892e37f204814afba2e7dc26617f9a0e
eshow <elvishehai521@163.com>
2025-07-23 11:03:12 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
   lang: 'en-US',
-  base:'eshow.github.io',
+  base:'/svip/',
   title: 'VuePress',
   description: 'My first VuePress Site',
 
